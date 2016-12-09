bca614adf7a47a66233c27d28373b9ace309f5b9
Liv brandon TON <liton@e3r4p11.42.fr>
2016-12-09 19:12:25 +0100

    nikeee

--- resolve_fillit.c
+++ resolve_fillit.c
@@ -6,7 +6,7 @@
 /*   By: liton <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/08 16:18:42 by liton             #+#    #+#             */
-/*   Updated: 2016/12/08 20:16:22 by liton            ###   ########.fr       */
+/*   Updated: 2016/12/09 18:19:49 by liton            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ char 	**resolve_fillit(char **map, int nb_t, tt_list *list)
 	begin = list;
 	while (count != nb_t)
 	{
+		/*
 		if (!map[i])
 		{
 			map = ft_map(size_map);
@@ -36,10 +37,13 @@ char 	**resolve_fillit(char **map, int nb_t, tt_list *list)
 			j = 0;
 			count = 0;
 			list = begin;
+			size_map++;
 		}
+		*/
 		if (map[i] && map[i][j] && map[i][j] == '.')
 		{
-			map = put_tetriminos(map, list, i, j);
+			if (!ttmn_in_map(map, list))
+				map = put_tetriminos(map, list, i, j);
 			if (ttmn_in_map(map, list))
 			{
 				list = list->next;
@@ -48,12 +52,19 @@ char 	**resolve_fillit(char **map, int nb_t, tt_list *list)
 			else
 				j++;
 		}
-		else if (map[i] && map[i][j] && map[i][j] != '.')
+		else if (map[i] && map[i][j])
 			j++;
 		else if (!map[i][j])
 		{
 			j = 0;
 			i++;
+		}
+		else if (!ttmn_in_map(map, list))
+		{
+			list = list->prev;
+			map = remove_tetriminos(map, list);
+			--count;
+			list = list->next;
 		}
 	}
 	return (map);
