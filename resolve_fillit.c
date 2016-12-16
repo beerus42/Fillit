f8c56cb03f1950064596fd7cbbae9484d8c21700
Liv brandon TON <liton@e3r6p13.42.fr>
2016-12-16 20:39:52 +0100

    push

--- resolve_fillit.c
+++ resolve_fillit.c
@@ -6,7 +6,7 @@
 /*   By: liton <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/08 16:18:42 by liton             #+#    #+#             */
-/*   Updated: 2016/12/16 05:20:55 by beerus           ###   ########.fr       */
+/*   Updated: 2016/12/16 20:39:32 by liton            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,94 +14,101 @@
 #include "../libft/libft.h"
 #include <stdlib.h>
 
-int		search_place(char ****map, t_ttmn *list)
+int		search_place(char **map, t_ttmn *list)
 {
 	int		i;
 	int		j;
 
 	i = -1;
-	if (!ttmn_in_map(**map, list))
+	if (!ttmn_in_map(map, list))
 	{	
-		while ((**map)[++i])
+		while (map[++i])
 		{
 			j = -1;
-			while ((**map)[i][++j])
+			while (map[i][++j])
 			{
-				if ((**map)[i] && (**map)[i][j] && (**map)[i][j] == '.')
+				if (map[i] && map[i][j] && map[i][j] == '.')
 				{
-					**map = put_tetriminos(**map, list, i, j);
-					if (ttmn_in_map(**map, list))
+					map = put_tetriminos(map, list, i, j);
+					if (ttmn_in_map(map, list))
 						return (1);
 				}
+//				ft_display_tab(map);
+//				ft_putchar('\n');
 			}
 		}
 	}
 	return (0);
 }
 
-int		last_ttmn(t_ttmn *list, char **map, t_ttmn *begin)
+int		put_max(char **map, t_ttmn **list, int *nb_t)
 {
-	int		last;
-	t_ttmn 	*tmp;
-
-	last = 0;
-	list = begin;
-	while (list)
+	while ((*list) && !ttmn_in_map(map, *list))
 	{
-		if (!ttmn_in_map(map, list))
-		{
-			if (list->nb == 1)
-				tmp = list;
-			if (last < list->nb)
-				last = list->nb;
-		}
-		list = list->next;
-	}
-	list = begin;
-	return (last);
-}
-
-int		put_in_order(char ***map, t_ttmn **list, int *count, t_ttmn *begin)
-{
-	int		cnt;
-	int		last;
-	int		order;
-
-	cnt = 0;
-	*list = begin;
-	order = 1;
-	last = last_ttmn(*list, *map, begin);
-	while (cnt != last)
-	{
-		while (((*list)->next && ttmn_in_map(*map, *list)) || (*list)->nb != order)
-			*list = (*list)->next;
-		if (search_place(&map, *list))
-		{
-			*list = begin;
-			++(*count);
-			++order;
-			cnt++;
-		}
-		else
+		if(!search_place(map, *list))
 			return (0);
+		*list = (*list)->next;
+		--(*nb_t);
 	}
 	return (1);
 }
 
-t_ttmn 		*change_order(char **map, t_ttmn *list, t_ttmn *begin)
+char	**reset_change_order(char **map, int **nb_t, t_ttmn **save, t_ttmn ***list)
 {
-	int		order;
-
-	order = 1;
-	list = begin;
-	while (list)
+	while ((**list)->next && ttmn_in_map(map, **list))
 	{
-		if (!ttmn_in_map(map, list))
-			list->nb = order++;
-		list = list->next;
+		map = remove_tetriminos(map, **list);
+		++(**nb_t);
+		**list = (**list)->next;
 	}
-	list = begin;
-	return (list);
+	*save = (*save)->next;
+	**list = *save;
+	return (map);
+}
+
+char		**reset(char **map, int **nb_t, t_ttmn *tmp, t_ttmn ***list)
+{
+	**list = tmp;
+	while (**list)
+	{
+		if (ttmn_in_map(map, **list))
+		{
+			map = remove_tetriminos(map, **list);
+			++(**nb_t);
+		}
+		**list = (**list)->next;
+	}
+	**list = tmp;
+	return (map);
+}
+
+int			resolve_fillit_2(char **map, int *nb_t, t_ttmn **list, t_ttmn *begin)
+{
+		t_ttmn 		*save;
+		t_ttmn		*tmp;
+
+		tmp = (*list)->prev;
+		save = *list;
+		while (nb_t)
+		{
+			if (search_place(map, *list) && --(*nb_t))
+			{
+				*list = begin;
+				while ((*list)->next && ttmn_in_map(map, *list))
+					*list = (*list)->next;
+			}
+			else
+			{
+				if (!(save->next) && nb_t)
+				{
+					map = reset(map, &nb_t, tmp, &list);
+					return (0);
+				}
+				*list = tmp;
+				map = reset_change_order(map, &nb_t, &save, &list);
+			}
+		}
+		return (1);
 }
 
 char		**resolve_fillit(char **map, int nb_t, t_ttmn *list)
@@ -113,11 +120,17 @@ char		**resolve_fillit(char **map, int nb_t, t_ttmn *list)
 	begin = list;
 	size_map = 5;
 	count = 0;
-	while (count != nb_t)
+	put_max(map, &list, &nb_t);
+	while (nb_t)
 	{
-		if (!put_in_order(&map, &list, &count, begin))
+		if (!ttmn_in_map(map, list) && nb_t)
 		{
-			list = change_order(map, list, begin);
+			map = remove_tetriminos(map, list->prev);
+			++nb_t;
+			if (!(resolve_fillit_2(map, &nb_t, &list, begin)) && nb_t)
+			{
+				size_map = 6;
+			}
 		}
 	}
 	return (map);
