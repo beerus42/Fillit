cb400a9be227f4c949c29ba7d1ad08b5c653abb1
Hakaishin berusu <beyy@uwrektbm.local>
2016-12-20 03:21:26 +0100

    ppush

--- resolve_fillit.c
+++ resolve_fillit.c
@@ -6,7 +6,7 @@
 /*   By: liton <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/08 16:18:42 by liton             #+#    #+#             */
-/*   Updated: 2016/12/17 01:43:13 by beerus           ###   ########.fr       */
+/*   Updated: 2016/12/17 03:49:25 by beerus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,7 @@ char	**reset_change_order(char **map, int **nb_t, t_ttmn **save, t_ttmn ***list)
 char		**reset(char **map, int **nb_t, t_ttmn *tmp, t_ttmn ***list)
 {
 	**list = tmp;
-	while (**list)
+	while ((**list)->next)
 	{
 		if (ttmn_in_map(map, **list))
 		{
@@ -85,6 +85,11 @@ char		**reset(char **map, int **nb_t, t_ttmn *tmp, t_ttmn ***list)
 			++(**nb_t);
 		}
 		**list = (**list)->next;
+	}
+	if (ttmn_in_map(map, **list))
+	{
+		map = remove_tetriminos(map, **list);
+		++(**nb_t);
 	}
 	**list = tmp;
 	return (map);
@@ -99,7 +104,7 @@ int			resolve_fillit_2(char **map, int *nb_t, t_ttmn **list, t_ttmn *begin)
 		save = *list;	
 		while (nb_t)
 		{
-			if (search_place(map, *list) && --(*nb_t))
+			if (search_place(map, *list) && (*nb_t)--)
 			{
 				*list = begin;
 				while ((*list)->next && ttmn_in_map(map, *list))
@@ -126,6 +131,7 @@ char		**resolve_fillit(char **map, int nb_t, t_ttmn *list)
 	int			size_map;
 	t_ttmn		*begin;
 	int			count;
+	t_ttmn		*tmp;
 
 	begin = list;
 	size_map = 5;
@@ -136,11 +142,13 @@ char		**resolve_fillit(char **map, int nb_t, t_ttmn *list)
 		if (!ttmn_in_map(map, list) && nb_t)
 		{
 			map = remove_tetriminos(map, list->prev);
+			tmp = list->prev;
 			++nb_t;
 			if (!(resolve_fillit_2(map, &nb_t, &list, begin)) && nb_t)
 			{
 				size_map = 6;
 			}
+			list = tmp;
 		}
 	}
 	return (map);
