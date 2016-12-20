e4d48d0cf21fbd24ea30cfc26c4367cf64e47997
Hakaishin berusu <beyy@uwrektbm.local>
2016-12-20 23:54:06 +0100

    e

--- re_fi.c
+++ re_fi.c
@@ -6,7 +6,7 @@
 /*   By: beerus <livbrandon@outlook.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/20 21:37:04 by beerus            #+#    #+#             */
-/*   Updated: 2016/12/20 21:50:37 by beerus           ###   ########.fr       */
+/*   Updated: 2016/12/20 22:50:36 by beerus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@ int		resolve_fillit_2(char **map, int *nb_t, t_ttmn *list)
 {
 	if (!search_place(map, list))
 		return (0);
-	++(*nb_t);
+	--(*nb_t);
 	if (list->next)
 		list = list->next;
-	while(nb_t && move_right(map, list->prev))
+	while(*nb_t && move_right(map, list->prev))
 	{
-		if (nb_t && search_place(map, list))
+		if (*nb_t && search_place(map, list) && (*nb_t)--)
 		{
 			if (list->next)
 				list->next;
+			else
+				return (1);
 		}
 	}
 	return (0);
